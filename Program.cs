866ba4990091061d6aab2b93bd764394820a0a4a
guilherme costa <guilhermecosta3572@gmail.com>
2021-01-15 02:45:12 -0300

    repetição maior menor finalizado

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Repeticao_Maior_Menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o menor valor lido.
+
+            // criar a lista
+            int tamanho= 5;
+            int[] lista = new int[tamanho];
+            long maiorValor = lista[0];
+            long menorValor = lista[0];
+
+            for(var i =0; i < lista.Length; i++) {
+
+                Console.Write($"Digite o {i+1}º valor: ");
+                lista[i] = int.Parse(Console.ReadLine()); 
+            }
+                
+                                
+            for(var i = 0; i < lista.Length; i++){
+            // se o valor atual da lista for maior que o valor que já tenho
+
+                if(lista[i] >= maiorValor){
+                
+                    maiorValor = lista[i];
+                    Console.WriteLine("O maior valor digitado foi: "+maiorValor);
+
+                }
+                else if(lista[i] <= menorValor)
+                {
+                    menorValor = lista[i];
+                    Console.WriteLine("O menor valor digitado foi: "+menorValor);
+                }else{
+                    Console.WriteLine("");
+                }
+            }    
+        } 
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Repeticao-Maior-Menor
+Faça um programa que leia 10 valores digitados pelo usuário e no final, escreva o maior e o menor valor lido.
